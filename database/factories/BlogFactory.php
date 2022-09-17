c20a4919725a2dd9172fad0b1e1b88d12d06d14c
koctaa04 <yeftaocta04@gmail.com>
2022-09-17 15:57:00 +0700

    asdnuasndiuas

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -19,7 +19,7 @@ class BlogFactory extends Factory
         return [
             'title' => $this->faker->sentence(mt_rand(2,8)),
             'slug' => $this->faker->slug(),
-            'excerpt' => $this->faker->paragraph(),
+            'excerpt' => $this->faker->sentence(mt_rand(8,10)),
             'body' => collect($this->faker->paragraphs(mt_rand(5,10)))->map(fn($p) => "<p>$p</p>")->implode(''),
             // 'user_id' => mt_rand(1,3)
         ];
